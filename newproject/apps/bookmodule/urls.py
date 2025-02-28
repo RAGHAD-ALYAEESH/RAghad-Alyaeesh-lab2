35ef826f278e3126cc52b32d82f1a9e040cfb6d1
RAGHAD-ALYAEESH <raghadyaeesh@gmail.com>
2025-02-28 12:09:41 +0300

    lab4

--- newproject/apps/bookmodule/urls.py
+++ newproject/apps/bookmodule/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('', views.index),
    re_path(r'index2/(?P<val1>\w+)/$', index2),  # يقبل أي نص أو رقم
    path('<int:bookId>', views.viewbook),
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
 
 
 ]
